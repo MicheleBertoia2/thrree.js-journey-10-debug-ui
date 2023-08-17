86437b76aa3214e11abdb88b7cd89eb38b88e022
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-08-17 18:05:52 +0200

    [add} lil-gui library and added tweaks

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
 import gsap from 'gsap'
+
+
+//Debug
+const gui = new GUI()
+
+const parameters = {
+    spin: () =>{
+        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2})
+    }
+}
+
 
 /**
  * Base
@@ -18,6 +30,31 @@ const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+//Debug
+// gui.add(mesh.position, 'y', -3, 3, 0.01)
+// gui.add(mesh.position, 'x', -3, 3, 0.01)
+// gui.add(mesh.position, 'z', -3, 3, 0.01)
+
+gui
+    .add(mesh.position, 'y')
+    .min(-3)
+    .max(3)
+    .step(0.01)
+    .name('elevation')
+
+gui 
+    .add(mesh, 'visible')
+
+gui
+    .add(material, 'wireframe')
+
+gui 
+    .addColor(material, 'color')
+
+gui
+    .add(parameters, 'spin')
+
 
 /**
  * Sizes
